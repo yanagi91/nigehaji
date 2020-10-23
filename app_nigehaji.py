5a878ec680710994482b91e011f284d7a1420cc3
yanagi91 <69830141+yanagi91@users.noreply.github.com>
2020-10-23 17:28:47 +0900

    Update app_nigehaji.py
    コメントの追加

--- app_nigehaji.py
+++ app_nigehaji.py
@@ -6,16 +6,18 @@ from pred2 import start_detect
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = './static/uploads'
-ALLOWED_EXTENSIONS = set(['jpg','png'])
+UPLOAD_FOLDER = './static/uploads' # 入力画像の保存場所
+ALLOWED_EXTENSIONS = set(['jpg','png']) # 拡張子の設定 ここで設定したものしか読み込まない
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = os.urandom(24)
 
 @app.context_processor
 def override_url_for():
+    """staticの画像の更新用"""
     return dict(url_for=dated_url_for)
 
 def dated_url_for(endpoint, **values):
+    # 判定後の画像を上書きしているため画像を更新する処理
     if endpoint == 'static':
         filename = values.get('filename', None)
         if filename:
@@ -34,42 +36,43 @@ def allwed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def uploads_file():
     if request.method == 'POST':
+        
+        # ファイルでないものが入力されたときの処理
         if 'file' not in request.files:
             flash('ファイルがありません', 'error')
             return redirect('/')
 
         file = request.files['file']
         print(file)
-        if file.filename == '':
+        
+        # ファイル名が入力されていないときの処理
+        if file.filename == '':            
             flash('ファイルがありません', 'error')
             return redirect('/')
         
+        # ファイルがあり拡張子が対応しているときの処理
         if file and allwed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
         select_name = request.form['select_name']
         #print(select_name)
-        if os.path.exists('static/dst/opencv_face_detect_rectangle1.jpg'):
-            os.remove('static/dst/opencv_face_detect_rectangle1.jpg')
         file_address = 'static/uploads/{}'.format(filename) # 判定画像アドレス
         
         predict_name, predict_enname, rate = start_detect(file_address, select_name) # 画像判定
 
         if predict_name != None:
+            # 選択した人物が画像から見つかった時の処理
             flash(predict_name, 'success')
             flash(rate + '%', 'success')
-            dst_img = 'dst/opencv_face_detect_rectangle1.jpg'
+            dst_img = 'dst/opencv_face_detect_rectangle1.jpg' # 選択した人物を四角で囲った画像
             return render_template('index.html', dst_img=dst_img)
         else:
+            # 選択した人物が画像から見つからなかった時の処理
             flash('検出できませんでした', 'error')
 
     return render_template('index.html')
 
-
-@app.route('/dst/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
